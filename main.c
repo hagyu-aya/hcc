7c459aad5be854379468e23f1573f1245925a7b5
hagyu_aya <aya.hagyu.6028@gmail.com>
2019-02-26 21:49:42 +0900

    加減算のプログラムのコンパイル

--- main.c
+++ main.c
@@ -3,14 +3,34 @@
 
 int main(int argc, char** argv){
 	if(argc != 2){
-		fprintf(stderr, "引数は数字1つのみです\n");
+		fprintf(stderr, "引数は1つのみです\n");
 		return 1;
 	}
 	
+	// アセンブリの表示
 	printf(".intel_syntax noprefix\n");
 	printf(".global main\n");
 	printf("main:\n");
-	printf("	mov rax, %d\n", atoi(argv[1]));
+
+	char *p = argv[1];
+	printf("	mov rax, %ld\n", strtol(p, &p, 10));
+	while(*p){
+		if(*p == '+'){
+			p++;
+			printf("	add rax, %ld\n", strtol(p, &p, 10));
+			continue;
+		}
+
+		if(*p == '-'){
+			p++;
+			printf("	sub rax, %ld\n", strtol(p, &p, 10));
+			continue;
+		}
+
+		fprintf(stderr, "+, -, 数字以外を入れないでください\n");
+		return 1;
+	}
+
 	printf("	ret\n");
 	return 0;
 }
